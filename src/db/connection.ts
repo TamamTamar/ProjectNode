8e6f287f063cf48fb966970f2ac8b29b266961c7
TamamTamar <tamar.tamam770@gmail.com>
2024-08-07 10:52:26 +0300

    connect

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,24 +1,29 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 import { Logger } from "../logs/logger";
 import initDB from "./init-db";
 
 const connect = async () => {
-  //load the value from .env
   const connectionString = process.env.DB_CONNECTION_STRING;
 
-  //check if the value exists
   if (!connectionString) {
     Logger.log("DB_CONNECTION_STRING is not defined in your .env file");
     return;
   }
+
   try {
-    await mongoose.connect(connectionString);
+    const options: ConnectOptions = {
+      serverSelectionTimeoutMS: 30000, // זמן המתנה לבחירת השרת
+      socketTimeoutMS: 45000, // זמן המתנה לשאילתות
+    };
+
+    await mongoose.connect(connectionString, options);
 
     await initDB();
 
     Logger.log("Database Connected");
   } catch (e) {
     Logger.log("Database not connected");
+    Logger.log(e);
   }
 };
 
