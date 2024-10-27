471956b7966e7c6b95f7dc819da09f8a85997bb9
TamamTamar <113338791+TamamTamar@users.noreply.github.com>
2024-10-27 20:51:09 +0200

    Update cart-router.ts

--- src/routes/cart-router.ts
+++ src/routes/cart-router.ts
@@ -20,9 +20,17 @@ router.get('/', validateToken, async (req, res, next) => {
 });
 
 //add to cart
-router.post('/add', ...validateAddToCart, async (req, res, next) => {
+router.post('/add', validateAddToCart, async (req, res, next) => {
     try {
-        const { productId, variantId, quantity, size} = req.body;
+        const { productId, variantId, quantity, size } = req.body;
+
+        // בדיקה אם המשתמש מחובר
+        if (!req.payload || !req.payload._id) {
+            console.log("User not authenticated, handling guest cart.");
+            // עבור אורחים, שולחים תגובה שתטופל בצד הקדמי (שמירה בלוקל סטורג')
+            return res.status(200).json({ message: "Guest user - handle in local storage" });
+        }
+
         const userId = req.payload._id;
         const cart = await cartService.addProductToCart(userId, productId, variantId, quantity, size);
         res.json(cart);
